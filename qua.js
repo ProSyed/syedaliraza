fedc7ced04f809080e5ed3693cc818c792063f2d
Domus <syedali.razashah1234@gmail.com>
2025-07-22 22:52:10 +0500

    Primary construction complete. First Testing: PASS

---
+++ qua.js
@@ -0,0 +1,7 @@
+var qua = {
+    Prime: "<p><b>Prime</b> is an upcoming 3-player parliamentary strategy web game. The game will be soon be hosted for all to enjoy.</p>",
+    Stakes: "<p><b>Coming Soon!</b></p><p>A quick single-player soccer betting game.</p>",
+    Code: "<p>The source code for my hobby coding projects.</p><p>Will be published soon.</p>",
+    Translations: "<p>My efforts at translating various works in eastern languages to English and each other.</p><p>Will be published soon.</p>",
+    Myself: "<p>Hey there,<br><b>Ali</b> here, a 20 year old learner from Punjab, Pakistan. This website serves as the portal to my current and future (if Allah wills) works and publications. Stay tuned, for there is much more to come.</p><p>Reach me out at:<br><i><a href='mailto:38syed@gmail.com'>38syed@gmail.com</a></i></p>"
+}
---
+++ run.js
@@ -0,0 +1,68 @@
+var menu = document.getElementById("menu");
+var thorax = document.getElementById("thorax");
+var thoraxTran = 0;
+var stamp = document.getElementById("stamp");
+var stampTran = 0;
+var heart = document.getElementById("heart");
+var heartTran = 0;
+var valve = document.getElementById("valve");
+var back = document.getElementById("back");
+
+function loadWindow() {
+    Object.keys(qua).forEach(member => {
+        const item = document.createElement("div");
+        item.classList.add('item');
+        item.setAttribute("onclick", "clickItem(this.innerHTML)");
+        item.innerHTML = member;
+        menu.append(item);
+    })
+}
+
+function clickItem(item) {
+    valve.innerHTML = qua[item];
+    thorax.style.width = "100%";
+    thoraxTran = 1;
+}
+
+function clickBack() {
+    thorax.style.maxHeight = 'var(--unit)';
+    heart.style.opacity = 0;
+    heartTran = 1;
+}
+
+function transitionThorax() {
+    if (thoraxTran == 1) {
+        stamp.style.opacity = 0;
+        stampTran = 1
+    }
+    else if (thoraxTran == 2) {
+        stamp.style.display = 'block';
+        stamp.offsetHeight;
+        stamp.style.opacity = 1;
+    }
+}
+
+function transitionStamp() {
+    if (stampTran == 1) {
+        stamp.style.display = 'none';
+        heart.style.display = 'block';
+        heart.offsetHeight;
+        thorax.style.maxHeight = heart.scrollHeight + 'px';
+        heart.style.opacity = 1;
+    }
+}
+
+function transitionHeart() {
+    if (heartTran == 1) {
+        heart.style.display = 'none';
+        heartTran = 0;
+        thorax.style.width = "50%";
+        thoraxTran = 2;
+    }
+}
+
+window.addEventListener("load", loadWindow);
+back.addEventListener("click", clickBack);
+thorax.addEventListener("transitionend", transitionThorax);
+stamp.addEventListener("transitionend", transitionStamp);
+heart.addEventListener("transitionend", transitionHeart);
